3731110f5100b71e7bc734f32b9627dedbf074f3
chiarafacciola <chiara.facciola@gmail.com>
2020-07-10 19:30:56 +0200

    written script for changing flow names

--- chiara-testing/rename-flows.js
+++ chiara-testing/rename-flows.js
@@ -5,12 +5,13 @@ var json_string = fs.readFileSync(input_path).toString();
 //var props= Object.getOwnPropertyNames(obj.flows[0])[0];
 var obj = JSON.parse(json_string);
 i = 0;
-var flows_list = {};
+var new_name ="";
 for (i = 0; i < obj.flows.length; i++) {
-  flows_list[obj.flows[i].name] = obj.flows[i].name;
+  new_name= "PLH - " + obj.flows[i].name;
+  obj.flows[i].name = new_name;
 }
-flows_list = JSON.stringify(flows_list, null, 2);
-var output_path = path.join(__dirname, "../santiago-testing/list_of_flows.json");
-fs.writeFile(output_path, flows_list, function (err, result) {
+new_flows= JSON.stringify(obj,null, 2);
+var output_path = path.join(__dirname, "../chiara-testing/renamed_flows.json");
+fs.writeFile(output_path, new_flows, function (err, result) {
   if (err) console.log('error', err);
 });
