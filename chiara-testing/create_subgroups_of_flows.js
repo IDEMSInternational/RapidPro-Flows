5d3d1ff08cc66e38f1cb464fb2d5eef1bf065556
chiarafacciola <chiara.facciola@gmail.com>
2020-07-20 15:36:32 +0200

    update

--- chiara-testing/create_subgroups_of_flows.js
+++ chiara-testing/create_subgroups_of_flows.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require("path");
-// var Math = require("math");
-var file_to_split = "malaysia-rapidpro-sandbox-for-testing2_07";
+
+var file_to_split = "malaysia-rapidpro-sandbox-for-testing20_07";
 var input_path = path.join(__dirname, "../products/covid-19-parenting/", file_to_split+ ".json");
 var json_string = fs.readFileSync(input_path).toString();
 var obj = JSON.parse(json_string);
@@ -10,9 +10,12 @@ var obj = JSON.parse(json_string);
 
 var fl;
 var N_FL_TOT = obj.flows.length;
-var n_group = 5;
-var N_FL_GR = Math.floor(N_FL_TOT/n_group); 
-var rem = N_FL_TOT%n_group;
+console.log(N_FL_TOT)
+var N_FL_GR = 4;
+var n_group = Math.floor(N_FL_TOT/N_FL_GR); 
+console.log(n_group)
+var rem = N_FL_TOT%N_FL_GR;
+console.log(rem)
 
 var gr;
 var fl;
@@ -38,7 +41,7 @@ for (gr = 1; gr <= n_group; gr++) {
  }
     
    if (rem > 0){
-    new_file = obj;
+    new_flows = Object.assign({}, obj);
     new_flows.flows =[];
     for (fl = 0; fl < rem; fl++){
         new_flows.flows.push(obj.flows[i]);
