c9bd3792e7a37b71e797855be716f776157d2c73
chiarafacciola <chiara.facciola@gmail.com>
2020-07-28 17:49:01 +0200

    updated script

--- scripts/create_file_for_translators.js
+++ scripts/create_file_for_translators.js
@@ -12,6 +12,7 @@ new_file = "";
 var localization;
 var bit;
 var i;
+var qr;
 
 
 for (fl in obj) {
@@ -20,24 +21,57 @@ for (fl in obj) {
         bit = localization[key_bit];
         if (bit.hasOwnProperty('text')){
             lines = bit.text[0].split("\n");
+            atom_to_translate = {};
+            atom_to_translate.has_extraline=0;
             for (i = 0; i < lines.length; i++){
+                if (lines[i]==""){
+                    atom_to_translate.has_extraline++;
+                } 
+                else {
+                    atom_to_translate.flow_id = fl;
+                    atom_to_translate.bit_id = key_bit;
+                    atom_to_translate.bit_type = "text";
+                    atom_to_translate.type_id = i;
+                    if (lines[i].startsWith("•\t")){
+                        atom_to_translate.text = lines[i].replace("•\t","");
+                        atom_to_translate.has_bullet = true;
+                    }
+                    else {
+                        atom_to_translate.text = lines[i];
+                    }
+                    new_file = new_file + JSON.stringify(atom_to_translate, null, 2);
+                    atom_to_translate = {};
+                    atom_to_translate.has_extraline=0;
+                }
+            }
+        }
+        if (bit.hasOwnProperty('quick_replies')){
+            for (qr = 0; qr < bit.quick_replies.length; qr++){
                 atom_to_translate = {};
                 atom_to_translate.flow_id = fl;
                 atom_to_translate.bit_id = key_bit;
-                atom_to_translate.bit_type = "text";
-                atom_to_translate.type_id = i;
-                if (lines[i].startsWith("•\t")){
-                    atom_to_translate.text = lines[i].replace("•\t","");
-                    atom_to_translate.has_bullet = "yes";
-                }
-                else {
-                    atom_to_translate.text = lines[i];
-                    atom_to_translate.has_bullet = "no";
-                }
+                atom_to_translate.bit_type = "quick_replies";
+                atom_to_translate.type_id = qr;
+                atom_to_translate.text = bit.quick_replies[qr];
+                atom_to_translate.note = "This is a quick reply and its traslation should be uniquely identified by the corresponding argument"
+                
                 new_file = new_file + JSON.stringify(atom_to_translate, null, 2);
             }
         }
-        
+        if (bit.hasOwnProperty('arguments')){
+            
+                atom_to_translate = {};
+                atom_to_translate.flow_id = fl;
+                atom_to_translate.bit_id = key_bit;
+                atom_to_translate.bit_type = "arguments";
+                atom_to_translate.type_id = "0";
+                atom_to_translate.text = bit.arguments[0];
+                atom_to_translate.note = "This is an argument and it may be used to identify a corresponding quick reply"
+                
+                new_file = new_file + JSON.stringify(atom_to_translate, null, 2);
+            
+        }
+
 
 
 
