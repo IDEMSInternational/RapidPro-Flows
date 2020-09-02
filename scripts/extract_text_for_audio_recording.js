7b8a56cb0c721dc6f11645b5207be00f19881fc9
chiarafacciola <chiara.facciola@gmail.com>
2020-09-02 19:12:55 +0200

    added key type = audio to each object

--- scripts/extract_text_for_audio_recording.js
+++ scripts/extract_text_for_audio_recording.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var path = require("path");
-var input_path = path.join(__dirname, "../products/covid-19-parenting/development/plh_master_for_audio_recording.json");
+var input_path = path.join(__dirname, "../products/covid-19-parenting/development/plh_master_no_quick_replies.json");
 var json_string = fs.readFileSync(input_path).toString();
 var obj = JSON.parse(json_string);
 
@@ -8,25 +8,21 @@ var bits_to_record = [];
 var new_bit = {};
 
 
-var fl;
-var n;
-var ac;
-
-var flow_id;
-var node_id;
-var msg_id;
 
 
 
-for (fl = 0; fl < obj.flows.length; fl++) {
-    for (n = 0; n < obj.flows[fl].nodes.length; n++) {
-        for (ac = 0; ac < obj.flows[fl].nodes[n].actions.length; ac++) {
-            curr_act = obj.flows[fl].nodes[n].actions[ac];
+
+
+for (var fl = 0; fl < obj.flows.length; fl++) {
+    for (var n = 0; n < obj.flows[fl].nodes.length; n++) {
+        for (var ac = 0; ac < obj.flows[fl].nodes[n].actions.length; ac++) {
+            var curr_act = obj.flows[fl].nodes[n].actions[ac];
             if (curr_act.type == "send_msg") {
                 new_bit.flow_id = obj.flows[fl].uuid;
                 new_bit.node_id = obj.flows[fl].nodes[n].uuid;
                 new_bit.msg_id = curr_act.uuid;
                 new_bit.text = curr_act.text;
+                new_bit.type = "audio";
                 bits_to_record.push(Object.assign({}, new_bit));
                 new_bit = {};
             }
@@ -36,7 +32,9 @@ for (fl = 0; fl < obj.flows.length; fl++) {
 }
 
 var to_record = JSON.stringify(bits_to_record, null, 2);
-    var output_path = path.join(__dirname, "../products/covid-19-parenting/development/audio-recording/eng/file_for_audio_recording_plh_master.json");
+//to_record = to_record.replace(/\\\"HelpMe\\\"/gi,'\\"HelpMe\\" (as one word)');
+
+var output_path = path.join(__dirname, "../products/covid-19-parenting/development/audio-recording/eng/file_for_audio_recording_plh_master.json");
     fs.writeFile(output_path, to_record, function (err, result) {
         if (err) console.log('error', err);
     });
